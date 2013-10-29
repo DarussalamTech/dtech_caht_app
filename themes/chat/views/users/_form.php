2779791deaef07c3054b46d79800abb2bca05646
tahir <tahir@darussalampk.com>
2013-10-29 14:52:31 +0500

    change

--- themes/chat/views/users/_form.php
+++ themes/chat/views/users/_form.php
@@ -15,47 +15,9 @@
 
 
     <h1><img src="<?php echo Yii::app()->theme->baseUrl; ?>/images/registration.png" /> Signup</h1>
-    <div class="name">
-        <div class="left">
-            Username:
-        </div>
-        <div class="right">
-            <input type="text" />
-        </div>
-    </div>
-    <div class="pswrd">
-        <div class="left">
-            Email: <span>Incorrect Email ID</span>
-        </div>
-        <div class="right">
-            <?php echo $form->textField($model, 'email'); ?>
-        </div>
-    </div>
-    <div class="pswrd">
-        <div class="left">
-            Email: <span>Incorrect Email ID</span>
-        </div>
-        <div class="right">
-            <?php echo $form->textField($model, 'email'); ?>
-        </div>
-    </div>
-    <div class="pswrd">
-        <div class="left">
-            Password: <span>Wrong Password</span>
-        </div>
-        <div class="right">
-            <?php echo $form->passwordField($model, 'password'); ?>
-        </div>
-    </div>
-    <div class="pswrd">
-        <div class="left">
-            Confirm Password: <span>Wrong Password</span>
-        </div>
-        <div class="right">
-            <?php echo $form->passwordField($model, 'retype'); ?>
-        </div>
-    </div>
-    <div class="row">
+   
+
+    <div class="row" style="margin-top:20px;">
         <?php echo $form->labelEx($model, 'email'); ?>
         <?php echo $form->textField($model, 'email'); ?>
         <?php echo $form->error($model, 'email'); ?>
